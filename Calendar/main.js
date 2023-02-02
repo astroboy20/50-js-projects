cbf9a8370a1e95f3f7e4c933471d18768b6b42e8
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-02-02 19:12:00 +0100

    first commit

---
+++ Calendar/main.js
@@ -0,0 +1,39 @@
+const monthEl = document.querySelector('.date h1')
+const fullDateEl = document.querySelector('.date p')
+const daysEl = document.querySelector('.days')
+const monthIndex = new Date().getMonth()
+const lastDay = new Date(new Date().getFullYear(),monthIndex +1,0).getDate()
+const firstDay = new Date(new Date().getFullYear(),monthIndex,1).getDay() -1
+
+const months = [
+    'Janauary',
+    'Febuary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+
+
+monthEl.innerHTML = `${months[monthIndex]}`
+fullDateEl.innerText = new Date().toDateString()
+
+let days = ''
+for (let i = firstDay; i > 0 ; i--){
+    days += `<div class="empty"></div>`
+}
+
+for (let i = 1; i <= lastDay ; i++){
+    i === new Date().getDate() ?
+    days += `<div class="today">${i}</div>` :
+    days += `<div>${i}</div>` 
+}
+
+daysEl.innerHTML = days
